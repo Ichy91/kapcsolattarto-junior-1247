f539a975ee3afdc4e7d4a4f7ce0a65d03ba2f2c3
Ichy91 <farago.istvan91@gmail.com>
2021-09-22 22:10:09 +0200

    feat: extend ContactsApiApplication.

--- src/main/java/hu/futureofmedia/task/contactsapi/ContactsApiApplication.java
+++ src/main/java/hu/futureofmedia/task/contactsapi/ContactsApiApplication.java
@@ -1,11 +1,27 @@
 package hu.futureofmedia.task.contactsapi;
 
+import hu.futureofmedia.task.contactsapi.repositories.CompanyRepository;
+import hu.futureofmedia.task.contactsapi.repositories.ContactRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class ContactsApiApplication {
+    @Autowired
+    private CompanyRepository companyRepository;
+    private ContactRepository contactRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(ContactsApiApplication.class, args);
+    }
+
+    @Bean
+    @Profile("prod")
+    public CommandLineRunner init() {
+        return args -> {};
     }
 }
