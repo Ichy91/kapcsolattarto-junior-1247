d4e115aaefb167a2987c3c24ca4be4ff3c62010a
Ichy91 <farago.istvan91@gmail.com>
2021-09-23 17:46:13 +0200

    feat: implement the update method.

--- src/main/java/hu/futureofmedia/task/contactsapi/services/ContactService.java
+++ src/main/java/hu/futureofmedia/task/contactsapi/services/ContactService.java
@@ -2,13 +2,13 @@ package hu.futureofmedia.task.contactsapi.services;
 
 import hu.futureofmedia.task.contactsapi.entities.Contact;
 import hu.futureofmedia.task.contactsapi.entities.StatusType;
+import hu.futureofmedia.task.contactsapi.model.TemporaryContact;
 import hu.futureofmedia.task.contactsapi.model.SimpleContact;
 import hu.futureofmedia.task.contactsapi.repositories.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import javax.persistence.OrderBy;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -41,8 +41,18 @@ public class ContactService {
 
     }
 
-    public void updateContactById() {
+    public void updateContactById(UUID uuid, TemporaryContact contact) {
+        Contact contactToUpdate = getContactById(uuid);
 
+        contactToUpdate.setUpdated_date(new Timestamp(System.currentTimeMillis()));
+        contactToUpdate.setLast_name(contact.getLast_name());
+        contactToUpdate.setFirst_name(contact.getFirst_name());
+        contactToUpdate.setEmail(contact.getEmail());
+        contactToUpdate.setPhone_number(contact.getPhone_number());
+        contactToUpdate.setCompany(contact.getCompany());
+        contactToUpdate.setStatus(contact.getStatus());
+
+        contactRepository.save(contactToUpdate);
     }
 
     public void deleteContactById(UUID uuid) {
