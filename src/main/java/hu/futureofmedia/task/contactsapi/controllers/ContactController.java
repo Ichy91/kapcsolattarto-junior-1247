5d94a4e111f145fa0e35ae68afa5f52c28e64e3e
Ichy91 <farago.istvan91@gmail.com>
2021-09-24 11:19:18 +0200

    feat: add endpoint for get one contact by id.

--- src/main/java/hu/futureofmedia/task/contactsapi/controllers/ContactController.java
+++ src/main/java/hu/futureofmedia/task/contactsapi/controllers/ContactController.java
@@ -1,5 +1,6 @@
 package hu.futureofmedia.task.contactsapi.controllers;
 
+import hu.futureofmedia.task.contactsapi.datahandler.DataHandler;
 import hu.futureofmedia.task.contactsapi.entities.Contact;
 import hu.futureofmedia.task.contactsapi.model.SimpleContact;
 import hu.futureofmedia.task.contactsapi.services.ContactService;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 @RestController
@@ -26,7 +28,8 @@ public class ContactController {
     }
 
     @GetMapping("/contact/{id}")
-    public Contact getCont(@PathVariable UUID id){
-        return contactService.getContactById(id);
+    public Map<String, String> getCont(@PathVariable UUID id){
+        Contact contact = contactService.getContactById(id);
+        return new DataHandler().createOneContactToJson(contact);
     }
 }
