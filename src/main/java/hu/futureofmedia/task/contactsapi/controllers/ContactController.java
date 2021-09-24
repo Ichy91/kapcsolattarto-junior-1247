74e99081f447ecd829e6c9314cdcbdf6a6cca152
Ichy91 <farago.istvan91@gmail.com>
2021-09-24 14:14:20 +0200

    feat: add endpoint for the statuses.

--- src/main/java/hu/futureofmedia/task/contactsapi/controllers/ContactController.java
+++ src/main/java/hu/futureofmedia/task/contactsapi/controllers/ContactController.java
@@ -3,15 +3,14 @@ package hu.futureofmedia.task.contactsapi.controllers;
 import hu.futureofmedia.task.contactsapi.datahandler.DataHandler;
 import hu.futureofmedia.task.contactsapi.entities.Company;
 import hu.futureofmedia.task.contactsapi.entities.Contact;
+import hu.futureofmedia.task.contactsapi.entities.StatusType;
 import hu.futureofmedia.task.contactsapi.model.SimpleContact;
 import hu.futureofmedia.task.contactsapi.services.CompanyService;
 import hu.futureofmedia.task.contactsapi.services.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 @RestController
 @RequestMapping("api/service")
@@ -36,6 +35,11 @@ public class ContactController {
     @GetMapping("/company/{id}")
     public Company getCompanyById(@PathVariable Long id) {
         return companyService.getCompanyById(id);
+    }
+
+    @GetMapping("/statuses")
+    public List<StatusType> getStatuses() {
+        return new ArrayList<>(Arrays.asList(StatusType.values()));
     }
 
     @GetMapping("/contacts")
