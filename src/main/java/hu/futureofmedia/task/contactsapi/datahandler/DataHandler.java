f460f957fce80b4ae4223492cc260e01d4a19eeb
Ichy91 <farago.istvan91@gmail.com>
2021-09-24 13:35:42 +0200

    feat: data handler class for create new contact.

---
+++ src/main/java/hu/futureofmedia/task/contactsapi/datahandler/DataHandler.java
@@ -0,0 +1,12 @@
+package hu.futureofmedia.task.contactsapi.datahandler;
+
+import hu.futureofmedia.task.contactsapi.entities.Contact;
+
+import java.util.Map;
+
+public class DataHandler {
+
+    public Contact createContact(Map<String, Object> data) {
+        return null;
+    }
+}
